c039721251332b4566bd48ffd4cd551c15e652c5
Admin <Admin@Admin-PC>
2018-09-14 10:46:44 +0530

    see this project

--- practise/src/com/prac/java/Demo.java
+++ practise/src/com/prac/java/Demo.java
@@ -1,24 +1,19 @@
 package com.prac.java;
 
 public class Demo {
-	private static Demo d;
-	private Demo() {
-		System.out.println("object created");
-	}
-	public static Demo getInstance() {
-		if(d==null) {
-			d=new Demo();
-		}
-		return d;
-	}
 	
 	public static void main(String[] args) {
-		Demo d1=Demo.getInstance();
-		Demo d2=Demo.getInstance();
-		Demo d3=Demo.getInstance();
-		Demo d4=Demo.getInstance();
+		int nor=5;
+		int row=1;
+		for (int i = nor; i >0; i--) {
+			for (int j = i; j >0; j--) {
+				System.out.print(j+" ");
+			}
+			for (int j = 1; j <=row; j++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+			row++;
+		}
 	}
-
 }
-
-
